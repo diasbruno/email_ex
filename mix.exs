04ce086679d3540a76eadb1dcd8021ef2ae36484
Bruno Dias <dias.h.bruno@gmail.com>
2019-11-02 23:17:27 -0300

    chore: update package information.

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule EmailEx.MixProject do
       app: :email_ex,
       version: "0.1.0",
       elixir: "~> 1.9",
+      description: "Address specification parser (RFC2822).",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -24,6 +25,13 @@ defmodule EmailEx.MixProject do
       {:combine, "~> 0.10.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package() do
+    [
+      links: %{"GitHub" => "https://github.com/diasbruno/email_ex"},
+      licenses: ["MIT"]
     ]
   end
 end
